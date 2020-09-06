81a853e31b54c11ba7173498d66e418d553271e6
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-09-06 02:27:34 -0500

    Assistant list

--- index.js
+++ index.js
@@ -3,8 +3,19 @@
  */
 require('dotenv').config()
 
-const Discord = require('discord.js')
-const client = new Discord.Client()
+// Extract the required classes from the discord.js module
+const { Client, MessageAttachment } = require('discord.js')
+const ObjectsToCsv = require('objects-to-csv')
+// Import the native fs module
+const fs = require('fs')
+
+// Create an instance of a Discord client
+const client = new Client()
+
+// important vars
+let assistanceList = false
+let assistances = []
+const fileName = './asistencia.csv'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -27,7 +38,47 @@ client.on('guildMemberAdd', member => {
 client.on('message', msg => {
   if (msg.channel.type === 'dm' && msg.author.id !== '746607629031440405') {
     // msg.reply('**AAAIMX**')
-    // client.channels.cache.find(ch => ch.name === 'bot').send(msg)
+    if (/!lista/.test(msg.content)) {
+      assistanceList = true
+      client.channels.cache
+        .find(ch => ch.name === 'bot')
+        .send(
+          'Inicia pase de **lista**.\nPor favor, escribe tu **nombre completo** en un solo mensaje'
+        )
+    } else if (/!stop/.test(msg.content)) {
+      console.log(assistances)
+      assistanceList = false
+
+      // If you use "await", code must be inside an asynchronous function:
+      ;(async () => {
+        const csv = new ObjectsToCsv(assistances)
+
+        // Save to file:
+        await csv.toDisk(fileName) // , { append: true }
+
+        // Return the CSV file as string:
+        const text = await csv.toString()
+
+        // Get the buffer from the 'asistencia.csv', assuming that the file exists
+        const buffer = fs.readFileSync(fileName)
+
+        /**
+         * Create the attachment using MessageAttachment,
+         * overwritting the default file name to 'memes.txt'
+         * Read more about it over at
+         * http://discord.js.org/#/docs/main/master/class/MessageAttachment
+         */
+        const attachment = new MessageAttachment(buffer, 'asistencia.csv')
+        msg.reply(text, attachment)
+        assistances = []
+      })()
+    }
+  }
+  if (assistanceList && msg.channel.name === 'bot' && msg.author.id !== '746607629031440405') {
+    const date = new Date(msg.createdTimestamp)
+    const person = { date: date.toLocaleString(), name: msg.content }
+    console.log(person)
+    assistances.push(person)
   }
 })
 
