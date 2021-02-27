487892f2e81dc57446ba81e23ca7bf47e09ab252
paulspartan14 <e17081416@itmerida.edu.mx>
2021-02-27 08:02:59 -0600

    added deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ~/production/aaaimx/aaaimx-discord
-sed -i "s/bot-aaimx:.*/bot-aaimx:$1/g" docker-compose.override.yaml
+sed -i "s/bot-aaaimx:.*/bot-aaaimx:$1/g" docker-compose.override.yaml
 docker-compose up -d
 yes | docker system prune -a
